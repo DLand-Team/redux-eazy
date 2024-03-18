73ad9268698104a7f2a44a6944bd0f8b1016ba21
qanglee <545540710@qq.com>
2024-03-18 13:31:35 +0800

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * from './hooks';
-export * from './utils';
-export * from './middleware';
-export { Provider } from 'react-redux';
-export { type PayloadAction } from '@reduxjs/toolkit';
+export * from "./hooks";
+export * from "./utils";
+export * from "./middleware";
+export { Provider } from "react-redux";
+export { type PayloadAction } from "@reduxjs/toolkit";
