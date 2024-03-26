913c3ed3bbb973b5b87614089788c1c3bea24cd7
qanglee <545540710@qq.com>
2024-03-26 14:16:50 +0800

    dev

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -52,21 +52,4 @@ export default defineConfig({
 			},
 		],
 	},
-	server: {
-		port: 8032,
-		proxy: {
-			"/api": {
-				// 配置需要代理的路径 --> 这里的意思是代理http://localhost:80/api/后的所有路由
-				target: "https://scaling.com.au/api", // 目标地址 --> 服务器地址
-				// target: 'http://localhost:4001/api', // 目标地址 --> 服务器地址
-				changeOrigin: true, // 允许跨域
-				ws: true, // 允许websocket代理
-				// 重写路径 --> 作用与vue配置pathRewrite作用相同
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-	},
-	preview: {
-		port: 8032,
-	},
 });
