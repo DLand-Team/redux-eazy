f49aaf8d72eab2b2eecd5eb7bd4f6b18bf58b031
qanglee <545540710@qq.com>
2024-03-26 14:15:54 +0800

    0.0.16

--- packages/core/src/middleware.ts
+++ packages/core/src/middleware.ts
@@ -1,23 +1,23 @@
 /* Core */
-import { createListenerMiddleware } from '@reduxjs/toolkit';
-import { createLogger } from 'redux-logger';
+import { createListenerMiddleware } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger";
 const listenerMiddleware = createListenerMiddleware();
 
 const middleware = [
-  listenerMiddleware.middleware,
-  createLogger({
-    duration: true,
-    timestamp: false,
-    collapsed: true,
-    colors: {
-      title: () => '#139BFE',
-      prevState: () => '#1C5FAF',
-      action: () => '#149945',
-      nextState: () => '#A47104',
-      error: () => '#ff0005',
-    },
-    predicate: () => typeof window !== 'undefined',
-  }),
+	listenerMiddleware.middleware,
+	createLogger({
+		duration: true,
+		timestamp: false,
+		collapsed: true,
+		colors: {
+			title: () => "#139BFE",
+			prevState: () => "#1C5FAF",
+			action: () => "#149945",
+			nextState: () => "#A47104",
+			error: () => "#ff0005",
+		},
+		predicate: () => typeof window !== "undefined",
+	}),
 ];
 
 export { listenerMiddleware, middleware };
